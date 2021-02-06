66dbfbbba2ddd28d8be185bf60aa5cbb71c5fa72
minjokim <upsk1@naver.com>
2021-02-06 12:12:28 +0900

    ADD:210206

---
+++ 프로그래머스/210206/괄호 변환.cpp
@@ -0,0 +1,58 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <stack>
+using namespace std;
+string global_p;
+string global_ans;
+stack<char> st;
+//설명에 따라 그대로 재귀 구현
+void go(int start, int end) {
+	if (start >= end)return;
+	if ((end - start + 1) == 2) {
+		global_ans += "()";
+		return;
+	}
+	int lcnt = 0, rcnt = 0;
+	bool flag = true;
+	//cout << start << ' ' << end << '\n';
+	for (int i = start; i <= end; i++) {
+		if (global_p[i] == '(') {
+			if (flag)st.push('(');
+			lcnt++;
+		}
+		else {
+			if (st.empty()) {
+				flag = false;
+			}
+			else st.pop();
+			rcnt++;
+		}
+		//cout << "확인 " << i << ' ' << flag << ' ' << lcnt << ' ' << rcnt << '\n';
+		if (lcnt == rcnt) {
+			if (flag) {
+				for (int j = start; j <= i; j++) {
+					global_ans += global_p[j];
+				}
+				go(i + 1, end);
+			}
+			else {
+				global_ans += '(';
+				go(i + 1, end);
+				global_ans += ')';
+				for (int j = start + 1; j < i; j++) {
+					if (global_p[j] == '(')global_ans += ')';
+					else global_ans += '(';
+				}
+			}
+			break;
+		}
+	}
+}
+string solution(string p) {
+	global_p = p;
+	global_ans = "";
+	go(0, p.size() - 1);
+	return global_ans;
+}
---
+++ 프로그래머스/210206/문자열 압축.cpp
@@ -0,0 +1,61 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//분할단위를 1~size/2 까지 나누어 브루트포스
+int solution(string s) {
+	int answer = 0;
+	answer = s.size();
+	int s2 = s.size() / 2;
+	//l= 분할단위
+	for (int l = 1; l <= s2; l++) {
+		string last = "";
+		int sum = 0, templ = 0;
+		int i2 = (s.size() / l) * l;
+		for (int i = 0; i < i2; i += l) {
+			int j2 = i + l;
+			string cur = "";
+			//cur에 현재 문자열 추가
+			//substr(i,l) 식으로 더 깔끔히 수정 가능
+			for (int j = i; j < j2; j++) {
+				cur += s[j];
+			}
+			if (cur == last) {
+				templ++;
+				if (j2 == i2) {
+					if (templ > 1) {
+						int templ2 = templ, cnt = 0;
+						while (templ2) {
+							templ2 /= 10;
+							cnt++;
+						}
+						sum += (last.size() + cnt);
+					}
+				}
+			}
+			else {
+				sum += last.size();
+				if (templ > 1) {
+					int templ2 = templ, cnt = 0;
+					while (templ2) {
+						templ2 /= 10;
+						cnt++;
+					}
+					sum += cnt;
+				}
+				last = cur;
+				templ = 1;
+				if (j2 == i2) {
+					sum += last.size();
+				}
+			}
+		}
+		sum += (s.size() - i2);
+		answer = min(answer, sum);
+	}
+	return answer;
+}
+int main() {
+	cout << solution("xxxxxxxxxxyyy");
+}
