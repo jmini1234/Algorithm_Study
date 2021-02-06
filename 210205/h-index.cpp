2f241d0119ffb2376c93806767de03efaebc2dec
arsgsg1 <arsgsg1@gmail.com>
2021-02-06 23:45:34 +0900

    [MOD] 210205

--- 210205/h-index.cpp
+++ 210205/h-index.cpp
@@ -3,14 +3,29 @@
 #include <functional>
 
 using namespace std;
-
+bool IsPossible(const vector<int>& v, int h)
+{
+    int cnt = 0;
+    for(const auto& item : v)
+        if(item >= h)
+            cnt++;
+    if(cnt >= h)
+        return true;
+    else
+        return false;
+}
 int solution(vector<int> citations) {
 	int answer = 0;
-	sort(citations.begin(), citations.end(), greater<int>());
-	if (!citations[0])        return 0;
-	for (int i = 0; i < citations.size(); i++) {
-		if (citations[i] > i)    answer++;
-		else    break;
-	}
-	return ++answer;
+    sort(citations.begin(), citations.end());
+    int left = 0, right = citations[citations.size() - 1];
+    int mid;
+    while(left <= right){
+        mid = (left + right) / 2;
+        if(IsPossible(citations, mid))
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    answer = right;
+	return answer;
 }
