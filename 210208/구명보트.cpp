117b31c50fcf2b2d94199904dda329115c6b30c3
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-02-08 08:29:32 +0900

    ADD : 210208

---
+++ 210208/구명보트.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(),people.end());
+    int s=0,e=people.size()-1;
+    while(s <= e){
+        if(people[s]+people[e] <= limit){
+            s++;
+        }
+        e--;
+        answer++;
+    }
+    return answer;
+}
