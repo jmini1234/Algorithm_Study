f6d7874802915773ea8da94f0f24e4da01afb949
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-02-12 15:36:16 +0900

    MOD : 210212

--- 210210/튜플.cpp
+++ 210210/튜플.cpp
@@ -2,55 +2,54 @@
 
 using namespace std;
 
-bool asc(vector<int> a, vector<int> b)
-{
-    return a.size() < b.size();
+
+int check[100001];
+
+bool cmp(vector<int> a, vector<int> b){
+    return a.size()<b.size();
 }
 
 vector<int> solution(string s) {
-    vector<int> answer;
-    bool check[100001] = {false, }, open = false;
-    vector<vector<int>> v;
-    vector<int> num;
-    string tmp = "";
+    vector<int> answer;   
+    vector<int> v;     
+    vector<vector<int>> rs;
+    string t="";
     
-    for(int i = 1; i < s.size() - 1; i++)
-    {
-        if(open == false && s[i] == '{')
-            open = true;
-        else if(open == true && (s[i] >= '0' && s[i] <= '9'))
-        {
-            tmp.push_back(s[i]);
+    for(int i=1;i<s.size()-1;i++){
+        
+        if(s[i] == '{') {
+            v.clear();
         }
-        else if(open == true && s[i] == ',')
-        {
-            int t = stoi(tmp);
-            num.push_back(t);
-            tmp = "";
-        }
-        else if(open == true && s[i] == '}')
-        {
-            int t = stoi(tmp);
-            num.push_back(t);
-            v.push_back(num);
-            open = false;
-            tmp = "";
-            num.clear();
-        }
-    }
-    
-    sort(v.begin(), v.end(), asc);
-    
-    for(int i = 0; i < v.size(); i++)
-    {
-        for(int j = 0; j < v[i].size(); j++)
-        {
-            if(check[v[i][j]] == false)
-            {
-                check[v[i][j]] = true;
-                answer.push_back(v[i][j]);
+        
+        else if(s[i] == ',') {
+            
+            if(s[i+1] == '{') continue;
+            else {
+                v.push_back(stoi(t));
+                t="";
             }
+            
+        }
+        else if(s[i] == '}') {
+            v.push_back(stoi(t));
+            t="";
+            rs.push_back(v);
+        }
+        else t+=s[i];
+    }
+    
+    sort(rs.begin(),rs.end(),cmp);
+    
+    for(int i=0;i<rs.size();i++){
+        for(int j=0;j<rs[i].size();j++){
+            
+            if(check[rs[i][j]] == 0){
+                check[rs[i][j]] = 1;
+                answer.push_back(rs[i][j]);
+            }
+            
         }
     }
+       
     return answer;
 }
