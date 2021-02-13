ee19f4dc3e42b5e70019ec113268c67c875e1398
minjokim <upsk1@naver.com>
2021-02-13 12:09:23 +0900

    ADD:210213

---
+++ 프로그래머스/210213/메뉴 리뉴얼.cpp
@@ -0,0 +1,54 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int l, ml;
+map<string, int> Map;
+map<string, bool> check;
+vector<string> order;
+vector<string> v[11][21];
+int MAX[11];
+//idx번째 주문, 주문의 음식, 현재 코스
+void go(int idx, int num, string now) {
+	if (now.size() > ml)return;
+	//최소 2가지 이상의 단품메뉴
+	if (!check[now]) {
+		check[now] = true;
+		if (now.size() > 1) {
+			Map[now]++;
+			int temp = Map[now];
+			//최소 2명이상의 주문
+			if (temp > 1) {
+				v[now.size()][temp].push_back(now);
+				MAX[now.size()] = max(MAX[now.size()], temp);
+			}
+		}
+	}
+	if (num == l)return;
+	go(idx, num + 1, now + order[idx][num]);
+	go(idx, num + 1, now);
+
+}
+vector<string> solution(vector<string> orders, vector<int> course) {
+	vector<string> answer;
+	order = orders;
+	int osize = order.size(), csize = course.size();
+	for (int i = 0; i < csize; i++) {
+		MAX[course[i]] = 0;
+	}
+	//ml은 course중 가장 긴 길이
+	ml = course[csize - 1];
+	for (int i = 0; i < osize; i++) {
+		sort(order[i].begin(), order[i].end());
+		//l은 order[i] 스트링의 길이
+		l = order[i].size();
+		check.clear();
+		go(i, 0, "");
+	}
+	for (int i = 0; i < csize; i++) {
+		for (string s : v[course[i]][MAX[course[i]]]) {
+			answer.push_back(s);
+		}
+	}
+	sort(answer.begin(), answer.end());
+	return answer;
+
+}
---
+++ 프로그래머스/210213/순위 검색.cpp
@@ -0,0 +1,109 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+map<string, int> Map;
+vector<string> temp[4];
+int cnt, ans;
+vector<int> v[24];
+int tempsize[4];
+string tempstring[4];
+void go(int idx, string now) {
+	if (idx == 4) {
+		Map[now] = cnt;
+		cnt++;
+		return;
+	}
+	for (int i = 0; i < 2; i++) {
+		go(idx + 1, now + temp[idx][i]);
+	}
+}
+string ansstring;
+void Find_ans(int idx, int MIN) {
+	if (idx == 4) {
+		//cout << ansstring << '\n';
+		int now_num = Map[ansstring];
+		auto p = lower_bound(v[now_num].begin(), v[now_num].end(), MIN);
+		ans += (v[now_num].end() - p);
+		return;
+	}
+	if (ansstring[idx] == '-') {
+		for (int i = 0; i < tempsize[idx]; i++) {
+			ansstring[idx] = tempstring[idx][i];
+			Find_ans(idx + 1, MIN);
+			ansstring[idx] = '-';
+		}
+	}
+	else {
+		Find_ans(idx + 1, MIN);
+	}
+}
+vector<int> solution(vector<string> info, vector<string> query) {
+	vector<int> answer;
+	//문자열 -> 숫자로 바꾸는 선처리
+	temp[0].push_back("c"); temp[0].push_back("j"); temp[0].push_back("p");
+	temp[1].push_back("b"); temp[1].push_back("f");
+	temp[2].push_back("j"); temp[2].push_back("s");
+	temp[3].push_back("c"); temp[3].push_back("p");
+	cnt = 0;
+	for (int i = 0; i < 3; i++) {
+		go(1, temp[0][i]);
+	}
+	tempsize[0] = 3; tempsize[1] = tempsize[2] = tempsize[3] = 2;
+	tempstring[0] = "cjp"; tempstring[1] = "bf"; tempstring[2] = "js"; tempstring[3] = "cp";
+	int isize = info.size(), qsize = query.size();
+	for (int i = 0; i < isize; i++) {
+		int num = 0;
+		string s = "";
+		//flag가 true면 직전 앞에 공백이며, 앞자리만 넣기 위해 쓸 임시 플래그
+		bool flag = true;
+		for (auto c : info[i]) {
+			if (c >= '0' && c <= '9') {
+				num *= 10;
+				num += (c - '0');
+				continue;
+			}
+			if (flag) {
+				s += c;
+			}
+			if (c == ' ')flag = true;
+			else flag = false;
+		}
+		v[Map[s]].push_back(num);
+	}
+	for (int i = 0; i < 24; i++)sort(v[i].begin(), v[i].end());
+	//for (int i = 0; i < 24; i++) {
+	//	cout << i << '\n';
+	//	for (auto k : v[i]) {
+	//		cout << k << ' ';
+	//	}
+	//	cout << '\n';
+	//}
+	for (int i = 0; i < qsize; i++) {
+		int num = 0;
+		string s = "";
+		//flag가 true면 직전 앞에 공백이며, 앞자리만 넣기 위해 쓸 임시 플래그
+		bool flag = true;
+		for (auto c : query[i]) {
+			if (c >= '0' && c <= '9') {
+				num *= 10;
+				num += (c - '0');
+				continue;
+			}
+			if (flag) {
+				if (c == 'a') {
+					flag = false;
+					continue;
+				}
+				s += c;
+			}
+			if (c == ' ')flag = true;
+			else flag = false;
+		}
+		ansstring = s;
+		ans = 0;
+		//cout << s << ' ' << num << '\n';
+		Find_ans(0, num);
+		answer.push_back(ans);
+		//cout << ans << '\n';
+	}
+	return answer;
+}
