bbbb92f5d221e0f7b0621c538e651ed33d38b5a4
심정민 <37237145+jmini1234@users.noreply.github.com>
2021-02-18 11:47:46 +0900

    Update 압축.cpp

--- 210218/압축.cpp
+++ 210218/압축.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-// 2분
 
 unordered_map<string,int> m;
 
@@ -35,8 +34,42 @@ vector<int> solution(string msg) {
             m.insert({tmp,idx++});
 
     }
-    
+    return answer;
+}
 
+/////////////////////////////////////////////////// 다른 풀이 //////////////////////////////////////////////////////////////////
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> v;
+
+vector<int> solution(string msg) {
+    vector<int> answer;
+    
+    for(int i=1;i<=26;i++){
+        string tmp;
+        tmp+='A'+i-1;
+        v.push_back(tmp);
+    }
+    
+    for(int i=0;i<msg.size();i++){
+        string tmp;
+        tmp+=msg[i];
+        int j;
+        int idx=i;
+        // 현재 문자열이 벡터에 존재하면
+        // 더해보기
+        while(find(v.begin(),v.end(),tmp)!=v.end()){
+            j = find(v.begin(),v.end(),tmp)-v.begin();
+            idx++;
+            tmp+=msg[idx];
+        }
+        i=idx-1;
+        // while을 사용해서 해당 문자열이 없으면 끝냄, 하지만 이 문자열을 다시 넣어주면 되기 때문에 tmp을 수정하지 않고 처리할 수 있다.
+        answer.push_back(j+1);
+        v.push_back(tmp);
+    }
     
     return answer;
 }
