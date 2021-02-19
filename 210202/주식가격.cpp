3aa8c7bb27465c5a9844b5f65a336f2b7d4f03f8
심정민 <shjj09@naver.com>
2021-02-19 11:30:52 +0900

    ADD:210219

--- 210202/주식가격.cpp
+++ 210202/주식가격.cpp
@@ -1,26 +1,83 @@
+#include<cmath>
+#include<iostream>
 #include <string>
-#include <vector>
-#include <stack>
+#include<map>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include <cstring>
+
 using namespace std;
-// 33분 stack으로 다시 풀기
-// 시간복잡도 : stack O(n), 이중 for문 O(n^2)
-// 일단 값이 감소하지 않은 방향으로 진행한다.
-vector<int> solution(vector<int> prices) {
-	// 각 인덱스별 주식 일수 저장
-	vector<int> answer(prices.size());
-	stack<int>st;
-	for (int i = 0; i<prices.size(); i++) {
-		//*새로 들어온 값에 의해 주식이 떨어져 answer이 결정되는 주식가격이 있는지 확인*
-		while (!st.empty() && prices[st.top()]>prices[i]) {
-			answer[st.top()] = i - st.top();
-			st.pop();
+// 16분
+int t;
+bool visited[10001] = { false, };
+int a, b;
+
+// 2 4 8 16
+// 2 4 8 
+
+int changeNum(int intNum, int idx) {
+	if (idx == 1) {
+		return 2 * intNum % 10000;
+	}
+	else if (idx == 2) {
+		if (intNum == 0)
+			return 9999;
+		else
+			return intNum - 1;
+	}
+	// R 오른쪽으로 옮기기
+	else if (idx == 3) {
+		int lastNum = intNum % 10;
+		intNum /= 10;
+		return lastNum * 1000 + intNum;
+	}
+	else {
+		int first = intNum / 1000;
+		intNum -= frist * 1000;
+		return first + intNum * 10;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> t;
+	// 현재 값, 명령어가 차례대로 저장
+	while (t--) {
+		memset(visited, false, sizeof(visited));
+		queue<pair<string, string>>q;
+		cin >> a >> b;
+		visited[a] = true;
+		// 무조건 BFS... 
+		// 하지만 명령어 나열 어케하지 .. (queue에 string으로 저장)
+		q.push({ a,"" });
+		while (!q.empty()) {
+			int cur = q.front().first;
+			int cmp = q.front().second;
+			q.pop();
+			if (cur == b) {
+				cout << cmp << endl;
+				break;
+			}
+			// 4가지 결과
+			for (int k = 1; k <= 4; k++) {
+				int changed = changeNum(cur, k);
+				if (changed<10000 && !visited[changed]) {
+					visited[changed] = true;
+					char ch;
+					if (k == 1) ch = 'D';
+					if (k == 2) ch = 'S';
+					if (k == 3) ch = 'L';
+					if (k == 4) ch = 'R';
+					if (changed == b) {
+						cout << cmp + ch << endl;
+						break;
+					}
+					q.push({ changed,cmp + ch });
+				}
+			}
 		}
-		st.push(i);
+
 	}
-	// 새로 들어온 값에 의해 주식이 떨어지지 않음, 즉 끝까지 주식이 떨어지지 않은 인덱스들
-	while (!st.empty()) {
-		answer[st.top()] = prices.size() - st.top() - 1;
-		st.pop();
-	}
-	return answer;
+
 }
